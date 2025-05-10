365a3496b71d7db58fe2ced9d2ccf2399fb9273e
JSSS <154106995+Sebys99@users.noreply.github.com>
2025-05-09 19:14:57 -0500

    Cierra chat

--- etapa1.js
+++ etapa1.js
@@ -1,5 +1,11 @@
-document.querySelectorAll(".close").forEach(function(element) {
-    element.addEventListener("click", function() {
-        document.getElementById("chat").style.display= "none";
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    var grupoClose = document.getElementById("close");
+
+    if (grupoClose) {
+        grupoClose.addEventListener("click", function() {
+            document.getElementById("chat").style.display = "none";
+        });
+    } else {
+        console.error("El elemento #close no se encuentra en el DOM.");
+    }
 });
